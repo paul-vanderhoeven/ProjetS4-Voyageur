807ad8806c4e1cfe7b2abe7549e55841f68a87fa
Paul vdh <paul.vdh@hotmail.fr>
2020-03-12 12:44:48 +0100

    Ajout de testAlgos14villes

--- src/main/java/projetS3Voyageur/App.java
+++ src/main/java/projetS3Voyageur/App.java
@@ -14,31 +14,22 @@ class App {
 
     public static void main(String[] args) throws Exception {
 
-        /*ModeRecherche[] algos = new ModeRecherche[6];
-        algos[0] = new BackTrackV2();
-        algos[1] = new BrutForceV4();
-        algos[2] = new PlusProcheV3();
-        algos[3] = new PPMulti();
-        algos[4] = new TrackProchesMulti();
-        algos[5] = new TrackProchesV2_1();
+        Point p1 = new Point(200, 0);
+        Point p2 = new Point(150, 150);
+        Point p3 = new Point(50, 250);
+        Point p4 = new Point(-100, 250);
+        Point p5 = new Point(-250, 150);
+        Point p6 = new Point(-350, 0);
+        Point p7 = new Point(-350, -200);
+        Point p8 = new Point(-300, -450);
+        Point p9 = new Point(-150, -550);
+        Point p10 = new Point(0, -550);
+        Point p11 = new Point(100, -500);
+        Point p12 = new Point(200, -400);
+        Point p13 = new Point(250, -200);
+        Point p14 = new Point(250, -50);
 
-        Analyser a = new Analyser(algos, 4, 1, 10);
-
-        a.analyse();
-
-        System.out.println(algos[0].getParcours().getVillesEmprunté());
-        System.out.println(algos[0].getParcours().toString());*/
-
-        Point p1 = new Point(100, 0);
-        Point p2 = new Point(50, 100);
-        Point p3 = new Point(-100, 150);
-        Point p4 = new Point(-200, 0);
-        Point p5 = new Point(-200, -150);
-        Point p6 = new Point(-100, -250);
-        Point p7 = new Point(50, -250);
-        Point p8 = new Point(150, -100);
-
-        double distance = p1.distance(p3) + p3.distance(p4) + p4.distance(p5) + p5.distance(p7) + p7.distance(p1);
+        double distance = p1.distance(p2) + p2.distance(p3) + p3.distance(p4) + p4.distance(p5) + p5.distance(p6) + p6.distance(p7) + p7.distance(p8) + p8.distance(p9) + p9.distance(p10) + p10.distance(p11) + p11.distance(p12) + p12.distance(p13) + p13.distance(p14) + p14.distance(p1);
         System.out.println(distance);
     }
 }
---
+++ src/test/java/ProjetS4/ModesDeRecherches/TestAlgos14villes.java
@@ -0,0 +1,100 @@
+package ProjetS4.ModesDeRecherches;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
+import projetS3Voyageur.CompositionPays.Pays;
+import projetS3Voyageur.ModesDeRecherches.*;
+
+import java.awt.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestAlgos14villes {
+
+    Point p1 = new Point(200, 0);
+    Point p2 = new Point(150, 150);
+    Point p3 = new Point(50, 250);
+    Point p4 = new Point(-100, 250);
+    Point p5 = new Point(-250, 150);
+    Point p6 = new Point(-350, 0);
+    Point p7 = new Point(-350, -200);
+    Point p8 = new Point(-300, -450);
+    Point p9 = new Point(-150, -550);
+    Point p10 = new Point(0, -550);
+    Point p11 = new Point(100, -500);
+    Point p12 = new Point(200, -400);
+    Point p13 = new Point(250, -200);
+    Point p14 = new Point(250, -50);
+    Pays p = new Pays(14);
+
+    @Before
+    public void init() {
+        p.setPositionVille(0, p1);
+        p.setPositionVille(1, p2);
+        p.setPositionVille(2, p3);
+        p.setPositionVille(3, p4);
+        p.setPositionVille(4, p5);
+        p.setPositionVille(5, p6);
+        p.setPositionVille(6, p7);
+        p.setPositionVille(7, p8);
+        p.setPositionVille(8, p9);
+        p.setPositionVille(9, p10);
+        p.setPositionVille(10, p11);
+        p.setPositionVille(11, p12);
+        p.setPositionVille(12, p13);
+        p.setPositionVille(13, p14);
+    }
+
+    @Test
+    public void test_14villes_backTrack() {
+        BackTrackV2 backTrackV2 = new BackTrackV2();
+        assertEquals(2275, test_algos_14villes(backTrackV2));
+    }
+
+    /*@Test
+    public void test_14villes_brutforce() {
+        BrutForceV2 brutForceV2 = new BrutForceV2();
+        assertEquals(2275, test_algos_14villes(brutForceV2));
+    }*/
+    @Test
+    public void test_14villes_PlusProche() {
+        PlusProcheV3 plusProcheV3 = new PlusProcheV3();
+        assertEquals(2275, test_algos_14villes(plusProcheV3));
+    }
+    @Test
+    public void test_14villes_PPMulti() {
+        PPMulti ppmulti = new PPMulti();
+        assertEquals(2275, test_algos_14villes(ppmulti));
+    }
+
+    @Test
+    public void test_14villes_PPMulti_plsrs_calcul() {
+        PPMulti ppmulti = new PPMulti();
+
+        ppmulti.recherche(p, 0);
+        ppmulti.recherche(p, 0);
+        ppmulti.recherche(p, 0);
+        ppmulti.recherche(p, 0);
+        ppmulti.recherche(p, 0);
+        ppmulti.recherche(p, 0);
+        ppmulti.recherche(p, 0);
+
+        assertEquals(2275, (int) ppmulti.getParcours().getDistance());
+    }
+    @Test
+    public void test_14villes_TrackProchesMulti() {
+        TrackProchesMulti trackProchesMulti = new TrackProchesMulti();
+        assertEquals(2275, test_algos_14villes(trackProchesMulti));
+    }
+    @Test
+    public void test_14villes_TrackProches() {
+        TrackProchesV2_1 trackProchesV2_1 = new TrackProchesV2_1();
+        assertEquals(2275, test_algos_14villes(trackProchesV2_1));
+    }
+
+    private int test_algos_14villes(ModeRecherche algo) {
+        algo.recherche(p, 0);
+        return (int) algo.getParcours().getDistance();
+    }
+}
