7993a3f4ae1e1e34b0a32540a281f464ba9bc052
Paul van der Hoeven <paul.vdh@hotmail.fr>
2020-03-16 16:18:05 +0100

    TestAlgos5villes

--- src/main/java/projetS3Voyageur/App.java
+++ src/main/java/projetS3Voyageur/App.java
@@ -1,12 +1,14 @@
 package projetS3Voyageur;
 
+import projetS3Voyageur.CompositionPays.Pays;
+
 import java.awt.*;
 
 class App {
 
     public static void main(String[] args) throws Exception {
 
-        Point p1 = new Point(200, 0);
+        /*Point p1 = new Point(200, 0);
         Point p2 = new Point(150, 150);
         Point p3 = new Point(50, 250);
         Point p4 = new Point(-100, 250);
@@ -23,6 +25,16 @@ class App {
         Point p15 = new Point(250, -50);
 
         double distance = p1.distance(p2) + p2.distance(p3) + p3.distance(p4) + p4.distance(p5) + p5.distance(p6) + p6.distance(p7) + p7.distance(p8) + p8.distance(p9) + p9.distance(p10) + p10.distance(p11) + p11.distance(p12) + p12.distance(p13) + p13.distance(p14) + p14.distance(p15) + p15.distance(p1);
+        System.out.println(distance);*/
+
+        Point p1 = new Point(100, 0);
+        Point p3 = new Point(-100, 150);
+        Point p5 = new Point(-200, -150);
+        Point p6 = new Point(-100, -250);
+        Point p7 = new Point(50, -250);
+        Pays p = new Pays(5);
+
+        double distance = p1.distance(p3) + p3.distance(p5) + p5.distance(p6) + p6.distance(p7) + p7.distance(p1);
         System.out.println(distance);
     }
 }
--- src/test/java/ProjetS4/ModesDeRecherches/TestAlgos5villes.java
+++ src/test/java/ProjetS4/ModesDeRecherches/TestAlgos5villes.java
@@ -11,15 +11,14 @@ import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
-public class TestAlgos6villes {
+public class TestAlgos5villes {
 
     Point p1 = new Point(100, 0);
     Point p3 = new Point(-100, 150);
-    Point p4 = new Point(-200, 0);
     Point p5 = new Point(-200, -150);
     Point p6 = new Point(-100, -250);
     Point p7 = new Point(50, -250);
-    Pays p = new Pays(6);
+    Pays p = new Pays(5);
 
     @Before
     public void init() {
@@ -27,7 +26,6 @@ public class TestAlgos6villes {
         ArrayList<Point> list = new ArrayList<>();
         list.add(p1);
         list.add(p3);
-        list.add(p4);
         list.add(p5);
         list.add(p6);
         list.add(p7);
@@ -40,37 +38,37 @@ public class TestAlgos6villes {
 
 
     @Test
-    public void test_6villes_backTrack() {
+    public void test_5villes_backTrack() {
         BackTrackV2 backTrackV2 = new BackTrackV2();
-        assertEquals(1126, test_algos_6villes(backTrackV2));
+        assertEquals(1112, test_algos_5villes(backTrackV2));
     }
     @Test
-    public void test_6villes_brutforce() {
+    public void test_5villes_brutforce() {
         BrutForceV2 brutForceV2 = new BrutForceV2();
-        assertEquals(1126, test_algos_6villes(brutForceV2));
+        assertEquals(1112, test_algos_5villes(brutForceV2));
     }
     @Test
-    public void test_6villes_PlusProche() {
+    public void test_5villes_PlusProche() {
         PlusProcheV3 plusProcheV3 = new PlusProcheV3();
-        assertEquals(1126, test_algos_6villes(plusProcheV3));
+        assertEquals(1112, test_algos_5villes(plusProcheV3));
     }
     @Test
-    public void test_6villes_PPMulti() {
+    public void test_5villes_PPMulti() {
         PPMulti ppmulti = new PPMulti();
-        assertEquals(1126, test_algos_6villes(ppmulti));
+        assertEquals(1112, test_algos_5villes(ppmulti));
     }
     @Test
-    public void test_6villes_TrackProchesMulti() {
+    public void test_5villes_TrackProchesMulti() {
         TrackProchesMulti trackProchesMulti = new TrackProchesMulti();
-        assertEquals(1126, test_algos_6villes(trackProchesMulti));
+        assertEquals(1112, test_algos_5villes(trackProchesMulti));
     }
     @Test
-    public void test_6villes_TrackProches() {
+    public void test_5villes_TrackProches() {
         TrackProchesV2_1 trackProchesV2_1 = new TrackProchesV2_1();
-        assertEquals(1126, test_algos_6villes(trackProchesV2_1));
+        assertEquals(1112, test_algos_5villes(trackProchesV2_1));
     }
 
-    private int test_algos_6villes(ModeRecherche algo) {
+    private int test_algos_5villes(ModeRecherche algo) {
         algo.recherche(p, 0);
         return (int) algo.getParcours().getDistance();
     }
